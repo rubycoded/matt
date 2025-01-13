5f7f25c5f362e000ecd10786d55aa1563d398db4
Jenxi <seow@jenxi.com>
2025-01-14 01:45:11 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,34 @@ export default {
   theme: {
     extend: {
       colors: {
-        'brand': '#ffd900',
-        'dark': '#21200b',
+        brand: {
+          DEFAULT: '#ffd900',
+          light: '#fff5b3',
+          lighter: '#fffae6',
+          dark: '#ccae00',
+          darker: '#997f00',
+        },
+        dark: {
+          DEFAULT: '#21200b',
+          light: '#363415',
+          lighter: '#4d4a1f',
+          dark: '#171608',
+          darker: '#0d0c04',
+        },
+        accent: {
+          orange: '#ff9900',
+          amber: '#ffb84d',
+          sage: '#9fa000',
+          olive: '#616119',
+          sand: '#f5e6c3',
+        },
       },
       fontFamily: {
         'body': ['Atkinson', 'sans-serif'],
+      },
+      maxWidth: {
+        'prose': '65ch',
+        'content': '42rem',
       },
     },
   },
