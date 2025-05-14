5cdd4104272fe5d7d4e98b8177ce05e9ab425812
Jenxi <seow@jenxi.com>
2025-05-14 15:38:47 +0800

    New design

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-
+import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://astronaut.github.io',
-	base: 'matt',
-	integrations: [mdx(), sitemap(), tailwind()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
