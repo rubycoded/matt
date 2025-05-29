0510a8f38dbcda81e6414aa72f3636753ba8d4e4
Jenxi <seow@jenxi.com>
2025-05-29 18:15:44 +0800

    Update config.ts

--- tina/config.ts
+++ tina/config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from "tinacms";
 
 // Your hosting provider likely exposes this as an environment variable
-const branch =
-  process.env.GITHUB_BRANCH ||
-  process.env.VERCEL_GIT_COMMIT_REF ||
-  process.env.HEAD
+const branch = process.env.CF_PAGES_BRANCH || "main"
 
 export default defineConfig({
   branch,
