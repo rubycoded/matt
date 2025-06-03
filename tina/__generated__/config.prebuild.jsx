a60b93fbe4bc6cb931b343a878217176453a629e
chungliwen <chungliwen@gmail.com>
2025-06-04 00:06:38 +0800

    Add TinaCMS generated files

---
+++ tina/__generated__/config.prebuild.jsx
@@ -0,0 +1,61 @@
+// tina/config.ts
+import { defineConfig } from "tinacms";
+var branch = process.env.CF_PAGES_BRANCH || "master";
+var config_default = defineConfig({
+  branch,
+  // Get this from tina.io
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  // Get this from tina.io
+  token: process.env.TINA_TOKEN,
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public"
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public"
+    }
+  },
+  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "src/content/blog",
+        format: "md",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true
+          },
+          {
+            type: "datetime",
+            name: "date",
+            label: "Date",
+            required: true
+          },
+          {
+            type: "datetime",
+            name: "modified",
+            label: "Modified",
+            required: false
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true
+          }
+        ]
+      }
+    ]
+  }
+});
+export {
+  config_default as default
+};
